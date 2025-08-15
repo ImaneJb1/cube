e8b8784b1fdd2b8a40b66bce5b58afe53441aa40
noussel <noussaiba.elkhadar@usmba.ac.ma>
2025-08-15 15:47:58 +0100

    remove all files

--- header.h
+++
@@ -1,8 +0,0 @@
-#ifndef HEADER_H
-#define HEADER_H
-#include "libft-project/libft.h"
-#include <fcntl.h>
-#include "get_next_line/get_next_line.h"
-int	ft_strcmp(const char *s1, const char *s2);
-
-#endif
