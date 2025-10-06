ffa005e7b32bb4a91114767beb22fe1ec1a1bd01
ImaneJb1 <imaane49@gmail.com>
2025-10-06 18:38:40 +0100

    add load textures

--- obj/parsing/global.d
+++
@@ -1,8 +0,0 @@
-obj/./parsing/global.o: parsing/global.c parsing/../header.h \
-  parsing/libft/libft.h parsing/get_next_line/get_next_line.h
-
-parsing/../header.h:
-
-parsing/libft/libft.h:
-
-parsing/get_next_line/get_next_line.h:
